15bcfe84b28e1be72ff7286faffea53feee098ac
shimakaze-git <shimakaze.soft+github@googlemail.com>
2019-01-09 01:38:31 +0900

    docs: create of Japanese translation environment

--- docs/conf.py
+++ docs/conf.py
@@ -334,3 +334,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/2': None}
+
+# locale
+locale_dirs = ['locale/']
+gettext_compact = False
